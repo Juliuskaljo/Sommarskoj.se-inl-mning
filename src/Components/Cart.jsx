db3ee00a652832dfa96a74f2d2e8a59f2badfa77
Julius Kaljo <julius.kaljo@hotmail.se>
2024-05-02 14:25:48 +0200

    added dlete function in cart

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -4,24 +4,37 @@ import './cart.css';
 
 const Cart = () => {
   const { cart } = useStore();
+  const { removeFromCart } = useStore();
 
   return (
     <div>
       <h2>Kundvagn</h2>
 	  
 	  <ul>
-		<div className='cart-grid'>
+		<div className='cart-flex'>
       
-        {cart.map((item, index) => (
+        {cart.map((toy, index) => (
           <li key={index}>
-			<img src={item.image} alt={item.name} />
-            <p>{item.name}</p>
-            <p>Färg: {item.color}</p>
-            <p>Pris: {item.price}Kr</p>
+			<img src={toy.image} alt={toy.name} />
+            <strong><p>{toy.name}</p></strong>
+            <p><strong>Färg:</strong> {toy.color}</p>
+            <p><strong>Pris:</strong> {toy.price}Kr</p>
+			<button onClick={() => removeFromCart(toy)}>Ta bort</button>
           </li>
         ))}
       </div>
 	 </ul> 
+
+	 <form>
+		<label className='cart-form'>
+			<input type="text" placeholder="Förnamn" />
+			<input type="text" placeholder='Efternamn' />
+			<input type="text" placeholder='Adress' />
+			<input type="text" placeholder='Telefonnummer' />
+		</label>
+		<button className='cart-button-accept'>Skicka beställning</button>
+		<button className='cart-button-decline'> Avbryt</button>
+	 </form>
     </div>
   );
 }
--- src/Data/store.js
+++ src/Data/store.js
@@ -1,10 +1,16 @@
 import {create} from 'zustand';
 
+//Kan ta bort enskilt produkt med samma innehåll tack vare date.now som Maja visade
 const useStore = create((set) => ({
     toyList: [],
-	cart: [],
+    cart: [],
     setToyList: (toyList) => set({ toyList }),
-	addToCart: (toy) => set((state) => ({ cart: [...state.cart, toy] })),
+    addToCart: (toy) => set((state) => ({
+        cart: [...state.cart, { ...toy, id: Date.now() }]
+    })),
+    removeFromCart: (toyToRemove) => set((state) => ({
+        cart: state.cart.filter((toy) => toy.id !== toyToRemove.id)
+    })),
 }));
 
 export { useStore };
