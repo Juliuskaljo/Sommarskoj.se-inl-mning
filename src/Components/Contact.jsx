5515fbd071dc656b10e4086508cee4772e2dbe41
Julius Kaljo <julius.kaljo@hotmail.se>
2024-05-05 17:47:56 +0200

    Added info in contact, and fixed som active on buttons

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -11,6 +11,23 @@ const Contact = () => {
 		Har du frågor, funderingar eller feedback? Tveka inte att kontakta oss! Vi finns här för att hjälpa dig och ser fram emot att höra från dig.
 		</p>
 
+		<p>
+		Du kan nå oss via e-post på:
+		</p>
+
+		<p>
+		Sommarskoj@info.se
+		</p>
+
+		<p>
+		eller ringa oss på:
+		</p>
+
+		<p>
+		08-123 456 78
+		</p>
+
+
 	</div>
   );
 }
